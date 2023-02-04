1739973de2100f54c37ea21b818b171fa30ed87b
EvgenyGrishchenkov <smp_mzmo@mail.ru>
2023-02-04 17:58:57 +0300

    Добавлена демонстрация работы SalesManager в класс Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] sales = {1, 3, 6, 10, 12, 1, 13};
+        SalesManager salesManager = new SalesManager(sales);
+        System.out.println(salesManager.max());
     }
 }
