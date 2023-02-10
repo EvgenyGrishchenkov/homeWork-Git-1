a882c658e04bfe3f89e4df512e8e7a83523b3219
EvgenyGrishchenkov <smp_mzmo@mail.ru>
2023-02-10 11:37:56 +0300

    Добавлены методы min и croppedMiddle

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int[] sales = {1, 3, 6, 10, 12, 1, 13};
+        int[] sales = {1, 3, 6, 10, 12, 15, 13};
         SalesManager salesManager = new SalesManager(sales);
         System.out.println(salesManager.max());
+        System.out.println(salesManager.min());
+        System.out.println(salesManager.croppedMiddle());
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,22 @@ public class SalesManager {
         }
         return max;
     }
+    public int min() {
+        int min = sales[0];
+        for (int i = 0;i<sales.length;i++) {
+            if (sales[i] >= 0 && sales[i] < min) {
+                min = sales[i];
+            }
+        }
+        return min;
+    }
+    public int croppedMiddle() {
+        int salesAmount = 0;
+        //int min = -1;
+        for (int sale : sales) {
+            salesAmount += sale;
+        }
+        int croppedMiddle = (salesAmount - max() - min())/(sales.length - 2);
+        return croppedMiddle;
+    }
 }
